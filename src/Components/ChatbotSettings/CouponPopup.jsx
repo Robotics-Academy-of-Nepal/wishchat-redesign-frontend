0609716735828e6f31111931e37444ed643444e5
_jATM0S <bks.mhryadayada@gmailcom>
2025-05-25 16:20:40 +0545

    chore:coupon esewa backend payment integrated

---
+++ src/Components/ChatbotSettings/CouponPopup.jsx
@@ -0,0 +1,107 @@
+import { useState } from "react";
+import { useChatbot } from "../../context/ChatbotContext";
+import { RiCoupon2Fill } from "react-icons/ri";
+import { LuArrowRight } from "react-icons/lu";
+import axiosInstance from "../../api/axiosInstance";
+
+const CouponPopup = ({
+  setPopUp,
+  selectedPlan,
+  setCoupon,
+  coupon,
+  handlePayment,
+}) => {
+  const { chatbotData } = useChatbot();
+  console.log("chatbot data in popup", chatbotData);
+
+  const [couponData, setCouponData] = useState();
+  const handleCoupon = () => {
+    // console.log(
+    //   "coupon trigerred:",
+    //   chatbotData.chatbot_id,
+    //   selectedPlan.price,
+    //   coupon,
+    //   subscription_plan_id
+    // );
+    axiosInstance
+      .post(`${import.meta.env.VITE_API_URL}auth/coupon/redeem/`, {
+        code: coupon,
+        chatbot_id: chatbotData.chatbot_id,
+        amount: selectedPlan.price,
+        subscription_plan_id: selectedPlan.id,
+      })
+      .then((response) => {
+        console.log(response);
+        setCouponData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div className="fixed z-50 inset-0 bg-black/50 flex justify-center items-center text-lg">
+      <div className="bg-white p-4 rounded-xl">
+        <h4 className=" mb-2">Redeem Coupon</h4>
+        <input
+          className=" border mb-2 font-mono p-2 rounded-lg"
+          placeholder="Your coupon here.."
+          value={coupon}
+          onChange={(e) => {
+            setCoupon(e.target.value);
+          }}
+        />
+
+        <div className="flex gap-2">
+          <button
+            className="border duration-500 transition-colors bg-white border-gray-400  text-gray-500 hover:text-white hover:bg-gray-400 font-bold py-1.5 px-4 rounded-lg"
+            onClick={() => {
+              setCoupon("");
+              setPopUp(false);
+            }}
+          >
+            Cancel
+          </button>
+
+          <button
+            className={` ${
+              coupon
+                ? "bg-blue-500 hover:text-blue-500 hover:bg-white"
+                : "bg-blue-400 cursor-not-allowed"
+            } border transition-colors duration-500   text-white font-bold py-1.5 px-4 rounded-lg flex items-center gap-2`}
+            title=""
+            onClick={handleCoupon}
+            disabled={!coupon}
+          >
+            Use
+            <RiCoupon2Fill className="h-4 w-4 " />
+          </button>
+        </div>
+
+        {couponData && (
+          <>
+            <div className="mb-2">
+              <p className="mt-2">Payable Amount</p>
+              <div className="border p-2 rounded-lg flex gap-2">
+                NPR{" "}
+                <s className="text-gray-500">{couponData.original_amount}</s>
+                {couponData.discounted_amount}
+              </div>
+            </div>
+            <button
+              title="Pay the subscription"
+              className="w-full flex gap-2 items-center justify-center py-1 px-2 bg-blue-500 text-white rounded-xl"
+              onClick={() =>
+                handlePayment(couponData.discounted_amount, selectedPlan.id)
+              }
+            >
+              Pay
+              <LuArrowRight />
+            </button>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+export default CouponPopup;
--- src/Components/ChatbotSettings/Payment.jsx
+++ src/Components/ChatbotSettings/Payment.jsx
@@ -2,108 +2,13 @@ import { LuArrowRight } from "react-icons/lu";
 import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
 import { RiCoupon2Fill } from "react-icons/ri";
-import { useChatbot } from "../../context/ChatbotContext";
 import axiosInstance from "../../api/axiosInstance";
-const PopUp = ({
-  setPopUp,
-  selectedPlan,
-  pricing,
-  setCoupon,
-  coupon,
-  handlePayment,
-}) => {
-  const { chatbotData } = useChatbot();
-  console.log("chatbot data in popup", chatbotData);
-  const requiredPlan = pricing.find((plan) => plan.title == selectedPlan);
-
-  const [couponData, setCouponData] = useState();
-  const handleCoupon = () => {
-    console.log(
-      "coupon trigerred:",
-      chatbotData.chatbot_id,
-      requiredPlan.price,
-      coupon
-    );
-    axiosInstance
-      .post(`${import.meta.env.VITE_API_URL}auth/coupon/redeem/`, {
-        code: coupon,
-        chatbot_id: chatbotData.chatbot_id,
-        amount: requiredPlan.price,
-      })
-      .then((response) => {
-        console.log(response);
-        setCouponData(response.data);
-      });
-  };
-
-  return (
-    <div className="fixed z-50 inset-0 bg-black/50 flex justify-center items-center text-lg">
-      <div className="bg-white p-4 rounded-xl">
-        <h4 className=" mb-2">Redeem Coupon</h4>
-        <input
-          className=" border mb-2 font-mono p-2 rounded-lg"
-          placeholder="Your coupon here.."
-          value={coupon}
-          onChange={(e) => {
-            setCoupon(e.target.value);
-          }}
-        />
-
-        <div className="flex gap-2">
-          <button
-            className="border duration-500 transition-colors bg-white border-gray-400  text-gray-500 hover:text-white hover:bg-gray-400 font-bold py-1.5 px-4 rounded-lg"
-            onClick={() => {
-              setCoupon("");
-              setPopUp(false);
-            }}
-          >
-            Cancel
-          </button>
-
-          <button
-            className={` ${
-              coupon
-                ? "bg-blue-500 hover:text-blue-500 hover:bg-white"
-                : "bg-blue-400 cursor-not-allowed"
-            } border transition-colors duration-500   text-white font-bold py-1.5 px-4 rounded-lg flex items-center gap-2`}
-            title=""
-            onClick={handleCoupon}
-            disabled={!coupon}
-          >
-            Use
-            <RiCoupon2Fill className="h-4 w-4 " />
-          </button>
-        </div>
-
-        {couponData && (
-          <>
-            <div className="mb-2">
-              <p className="mt-2">Payable Amount</p>
-              <div className="border p-2 rounded-lg flex gap-2">
-                NPR{" "}
-                <s className="text-gray-500">{couponData.original_amount}</s>
-                {couponData.discounted_amount}
-              </div>
-            </div>
-            <button
-              title="Pay the subscription"
-              className="w-full flex gap-2 items-center justify-center py-1 px-2 bg-blue-500 text-white rounded-xl"
-              onClick={() => handlePayment(couponData.discounted_amount)}
-            >
-              Pay
-              <LuArrowRight />
-            </button>
-          </>
-        )}
-      </div>
-    </div>
-  );
-};
+import CouponPopup from "./CouponPopup";
 
 const Payment = () => {
   const [popUpOpen, setPopUp] = useState(false);
   const [coupon, setCoupon] = useState("");
-  const [selectedPlan, setSelectedPlan] = useState("");
+  const [selectedPlan, setSelectedPlan] = useState({});
 
   const [pricing, setPricingDetail] = useState([]);
   useEffect(() => {
@@ -123,12 +28,13 @@ const Payment = () => {
     fetchData();
   }, []);
 
-  const handlePayment = (totalAmount, plan_id) => {
+  const handlePayment = (totalAmount, subscription_plan_id) => {
+    console.log("handlePayment", totalAmount, subscription_plan_id);
     axiosInstance
       .post("api/initiate-esewa-payment/", {
         total_amount: totalAmount,
         coupon: coupon,
-        plan_id: plan_id,
+        subscription_plan_id: subscription_plan_id,
       })
       .then((response) => {
         console.log(response);
@@ -142,10 +48,9 @@ const Payment = () => {
     <div className="w-full p-4 ">
       <h3 className="text-2xl font-semibold text-center mb-4">Payment</h3>
       {popUpOpen && (
-        <PopUp
+        <CouponPopup
           setPopUp={setPopUp}
           selectedPlan={selectedPlan}
-          pricing={pricing}
           setCoupon={setCoupon}
           coupon={coupon}
           handlePayment={handlePayment}
@@ -195,7 +100,7 @@ const Payment = () => {
                   <button
                     className="absolute top-0 right-0 bg-indigo-100 text-blue-700 font-semibold px-4 py-2 text-sm rounded-full shadow-sm border border-indigo-300 flex items-center gap-2 hover:bg-indigo-200 transition-all"
                     onClick={() => {
-                      setSelectedPlan(plan.title);
+                      setSelectedPlan(plan);
                       setPopUp(true);
                     }}
                   >
